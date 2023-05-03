ff8d4e87ce4491ad8ab02530116aec46581e0d28
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-05-03 17:44:57 -0400

    middleware finished

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
   console.log(
@@ -9,16 +11,48 @@ function logger(req, res, next) {
   next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({
+        message: 'No such user'
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'Problem finding user'
+    })
+  }
 }
 
+
+
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: 'Missing required name field'
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const { text } = req.body
+  if (!text || !text.trim()) {
+    res.status(400).json({
+      message: 'Missing required name field'
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
